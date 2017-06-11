e13758c67d967e5fab8871f88e9398495d90c193
emilsifu <emil_sifu@yahoo.com>
2017-06-11 21:04:15 +0300

    modification
    modification 11.06.2017

---
+++ app/Http/Controllers/Hello.php
@@ -0,0 +1,37 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class Hello extends Controller
+{
+    public function index(){
+
+        return 'Hello World from controller';
+    }
+
+    public function create(){
+
+    }
+
+    public function store(Request $request){
+
+    }
+
+    public function show($name){
+    return view('hello', array('name' => $name));
+    }
+
+    public function edit($id){
+
+    }
+
+    public function update(Request $request,$id){
+
+    }
+
+    public function destroy($id){
+
+    }
+}
